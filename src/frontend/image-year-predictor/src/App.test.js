bce4f63a66a725389786c45d83dfd81896f704b9
Josha G Thomas <joshathomas@g.harvard.edu>
2024-11-16 19:44:28 -0500

    Update App.test.js

--- src/frontend/image-year-predictor/src/App.test.js
+++ src/frontend/image-year-predictor/src/App.test.js
@@ -5,20 +5,11 @@ beforeAll(() => {
   window.alert = jest.fn(); // Mock alert
 });
 
-test('renders the banner title and description', async () => {
-  render(<App />);
-  const titleElement = await screen.findByText(/DecAide: The Virtual Fashion Historian/i);
-  const descriptionElement = await screen.findByText(/This tool is for celebrity stylists/i);
-
-  expect(titleElement).toBeInTheDocument();
-  expect(descriptionElement).toBeInTheDocument();
-});
-
 test('ensures the Predict Year button is disabled after click', async () => {
   render(<App />);
   const predictButton = screen.getByText(/Predict Year/i);
 
-  // Verify the button starts enabled
+  // Verify the button starts as enabled
   expect(predictButton).not.toBeDisabled();
 
   // Simulate clicking the Predict Year button
@@ -34,7 +25,7 @@ test('ensures the Generate Caption button is disabled after click', async () => 
   render(<App />);
   const captionButton = screen.getByText(/Generate Caption/i);
 
-  // Verify the button starts enabled
+  // Verify the button starts as enabled
   expect(captionButton).not.toBeDisabled();
 
   // Simulate clicking the Generate Caption button
