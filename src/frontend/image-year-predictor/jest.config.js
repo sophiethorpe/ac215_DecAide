95a39fdf7de55670532c34184444e24a1cb80813
Josha-G-Thomas <joshathomas@g.harvard.edu>
2024-11-17 14:06:32 -0500

    updated config

---
+++ src/frontend/image-year-predictor/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  collectCoverage: true,                // Enable coverage collection
+  coverageDirectory: 'coverage',       // Directory where the coverage files will be saved
+  coverageReporters: ['json'],         // Generate coverage in JSON format
+};
+
